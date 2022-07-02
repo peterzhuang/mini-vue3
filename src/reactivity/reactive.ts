f38744d5a7d612d55f2e779ac39288e244a44410
Yuchuan <yuchuan@example.com>
2022-07-02 16:59:52 -0400

    06 - impl reactive/effect func

---
+++ src/reactivity/reactive.ts
@@ -0,0 +1,21 @@
+import { track, trigger } from './effect';
+
+export function reactive(raw) {
+    
+    return new Proxy(raw, {
+        get(target, key) {
+            const res = Reflect.get(target, key);
+
+            //Todo: collect dependencies
+            track(target, key);
+            return res;
+        },
+        set(target, key, value) {
+            const res = Reflect.set(target, key, value);
+            
+            //Todo: trigger dependencies
+            trigger(target, key);
+            return res;
+        } 
+    });
+}
